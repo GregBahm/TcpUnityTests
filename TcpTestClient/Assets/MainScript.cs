44f8983231b8da4f24dcccffd83ccaa56b91f9eb
Greg Bahm <gregbahm@gmail.com>
2017-10-30 18:49:12 -0700

    Initial test of the communication functionaity.

---
+++ TcpTestClient/Assets/MainScript.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour
+{
+    public Material Mat;
+    private TcpClient client;
+    private NetworkStream stream;
+    private Texture2D textureVessel;
+
+    private byte[] dataToPublish = null;
+
+    IEnumerator Start()
+    {
+        textureVessel = new Texture2D(1024, 1024);
+        
+        byte[] header = new byte[4];
+        byte[] data = new byte[2000000];
+        while (true)
+        {
+            // Update() coroutine starts here:
+
+            client = new TcpClient();
+            client.Connect("127.0.0.1", 1990);
+
+            stream = client.GetStream();
+
+            // Get length
+            int headerLength = stream.Read(header, 0, header.Length);
+            Debug.Assert(headerLength == 4);
+            int imageLength = BitConverter.ToInt32(header, 0);
+
+            // Get data
+            int offset = 0;
+            while (offset < imageLength)
+            {
+                if (offset != 0)
+                {
+                    // Wait a frame if we have to read more than once
+                    yield return null;
+                }
+
+                offset += stream.Read(data, offset, data.Length - offset);
+            }
+
+            Debug.Log(offset);
+            Debug.Log(stream.DataAvailable);
+            textureVessel.LoadImage(data);
+            Mat.SetTexture("_MainTex", textureVessel);
+            stream.Close();
+            client.Close();
+
+            yield return null;
+        }
+    }
+}
---
+++ TcpTestServer/TcpTestServer/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpTestServer
+{
+    class Program
+    {
+        private static TcpListener server;
+        private static string dataFolder = @"D:\PlaceViewer\ProcessedData";
+        private static string[] filePaths;
+        private static int i;
+        private static byte[] image;
+
+        static void Main(string[] args)
+        {
+            filePaths = Directory.GetFiles(dataFolder);
+
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+            server = new TcpListener(localAddr, 1990);
+            server.Start();
+
+            while (true)
+            {
+                image = GetNextImageData();
+                Console.WriteLine("Waiting for a connection");
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Connected!");
+                
+                NetworkStream stream = client.GetStream();
+
+                byte[] bytes = BitConverter.GetBytes(image.Length);
+                Debug.Assert(bytes.Length == 4);
+                stream.Write(bytes, 0, bytes.Length);
+
+                stream.Write(image, 0, image.Length);
+                Console.WriteLine("Sent image " + i + ", which was " + image.Length + " bytes");
+                client.Close();
+            }
+        }
+
+        private static byte[] GetNextImageData()
+        {
+            i++;
+            string filePath = filePaths[i];
+            return File.ReadAllBytes(filePath);
+        }
+    }
+}
